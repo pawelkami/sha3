62ab386ad384437d7381a57ca48dc76c21c5cb0e
janmuczy <jmuczyns@mion.elka.pw.edu.pl>
2017-10-19 20:14:47 +0200

    Dodano plik z klasa odpowiedzialna za narysowanie okna aplikacji i pobrania danych wejsciowych oraz wywowalania odpowiedniej funkcji i wyswietlenia jej wyniku uzytkownikowi

---
+++ main.py
@@ -0,0 +1,148 @@
+import tkinter as tk
+from tkinter import filedialog
+from functools import partial
+
+'''
+Class liable to draw window with possibility to specify file to hash
+'''
+class UI(tk.Frame):
+
+    '''
+    Constructor
+    '''
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.initWindow(parent)
+
+        self.algorithms = {
+            1 : partial(UI.sha224),
+            2 : partial(UI.sha256),
+            3 : partial(UI.sha384),
+            4 : partial(UI.sha512),
+        }
+
+    '''
+    Method liable to draw all buttons and textboxes
+    '''
+    def initWindow(self, parent):
+        self.sha_opt = tk.IntVar()
+        self.sha_opt.set(1)
+        self.exported = tk.IntVar()
+
+        self.file = tk.StringVar()
+
+        self.infoText = tk.Label(self, text="info")
+        self.infoText.bind("<Button-1>", self.info)
+        self.prompt = tk.Label(self, text="Pick file to compute hash SHA3")
+
+        self.path = tk.Entry(self, textvariable=self.file, width=40)
+        self.path.config(state='readonly')
+
+        self.exploreBtn = tk.Button(self, text="Explore", command=self.pickFile)
+        self.computeBtn = tk.Button(self, text="Compute", command=self.compute)
+
+        self.sha224Btn = tk.Radiobutton(self, text="224 bit", variable=self.sha_opt, value=1)
+        self.sha256Btn = tk.Radiobutton(self, text="256 bit", variable=self.sha_opt, value=2)
+        self.sha384Btn = tk.Radiobutton(self, text="384 bit", variable=self.sha_opt, value=3)
+        self.sha512Btn = tk.Radiobutton(self, text="512 bit", variable=self.sha_opt, value=4)
+
+        # lay the widgets out on the screen.
+        tk.Label(self, text="").grid(row=0, column=0, columnspan=6)
+
+        self.prompt.grid(row=1, column=1, columnspan=4)
+        self.infoText.grid(row=1, column=6)
+
+        tk.Label(self, text="").grid(row=2, column=0, columnspan=6)
+
+        self.path.grid(row=3, column=1, columnspan=6, padx=5, sticky=tk.W)
+        self.exploreBtn.grid(row=3, column=6)
+
+        tk.Label(self, text="").grid(row=5, column=0, columnspan=6)
+
+        self.sha224Btn.grid(row=6, column=2)
+        self.sha256Btn.grid(row=6, column=3)
+        self.sha384Btn.grid(row=6, column=4)
+        self.sha512Btn.grid(row=6, column=5)
+        self.computeBtn.grid(row=6, column=6)
+
+
+    '''
+    Method draws window where user could pick file
+    '''
+    def pickFile(self):
+        filename = filedialog.askopenfilename()
+        self.file.set(filename)
+
+
+    '''
+    Method is liable to print information about program
+    '''
+    def info(self, event):
+        dialog = tk.Toplevel()
+        ws = root.winfo_screenwidth()
+        hs = root.winfo_screenheight()
+        w = 280
+        h = 100
+        dialog.geometry("%dx%d+%d+%d" % (w, h, ws / 2 - w / 2, hs / 2 - h / 2))
+        dialog.title('Info')
+        toDisplay = """Application computing value of SHA3 hash of given file"""
+        msg = tk.Message(dialog, text=toDisplay, width=220)
+        msg.pack()
+        tk.Label(dialog, text="").pack()
+        button = tk.Button(dialog, text="  OK  ", command=dialog.destroy)
+        button.pack()
+
+    '''
+    Print message box with hash value
+    '''
+    def printResult(self, hash):
+        dialog = tk.Toplevel()
+        ws = root.winfo_screenwidth()
+        hs = root.winfo_screenheight()
+        w = 280
+        h = 130
+        dialog.geometry("%dx%d+%d+%d" % (w, h, ws / 2 - w / 2, hs / 2 - h / 2))
+        dialog.title('Result')
+        toDisplay = str(hash)
+        msg = tk.Message(dialog, text=toDisplay, width=220)
+        msg.pack()
+        tk.Label(dialog, text="").pack()
+        button = tk.Button(dialog, text="  OK  ", command=dialog.destroy)
+        button.pack()
+
+    '''
+    Method is liable to start appropriate algorithm SHA3 version
+    '''
+    def compute(self):
+        # print(self.file.get())
+        # print(self.sha_opt.get())
+        sha = self.algorithms[self.sha_opt.get()]
+        hash = sha(self.file.get())
+        self.printResult(hash)
+
+    @staticmethod
+    def sha224(file):
+        return 224
+
+    @staticmethod
+    def sha256(file):
+        return 256
+
+    @staticmethod
+    def sha384(file):
+        return 384
+
+    @staticmethod
+    def sha512(file):
+        return 512
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.wm_title('SHA3 Algorithm')
+    ws = root.winfo_screenwidth()
+    hs = root.winfo_screenheight()
+    w = 320
+    h = 150
+    root.geometry("%dx%d+%d+%d" % (w, h, ws / 2 - w / 2, hs / 2 - h / 2))
+    UI(root, ).pack(fill="both", expand=True)
+    root.mainloop()
