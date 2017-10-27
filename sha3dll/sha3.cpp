348a309480923bd6fbdc4ebe5b9cac4dd51a0f15
janmuczy <jmuczyns@mion.elka.pw.edu.pl>
2017-10-28 00:39:22 +0200

    Dodano metody do konwersji stringa na tablice stanu i odwrotnie

--- sha3dll/sha3.cpp
+++ sha3dll/sha3.cpp
@@ -2,6 +2,35 @@
 
 
 
+void sha3::convertStringToStateArray(const std::vector<char>& str)
+{
+	for (unsigned x = 0; x < X_MAX; ++x)
+		for (unsigned y = 0; y < Y_MAX; ++y)
+			for (unsigned z = 0; z < W_MAX; ++z)
+				A[x][y][z] = str[W_MAX * (5 * y + x) + z];
+}
+
+std::vector<char> sha3::convertStateArrayToString()
+{
+	std::vector<char> str;
+
+	std::array<std::array<std::vector<char>, Y_MAX>, X_MAX> lane;
+	for (unsigned i = 0; i < X_MAX; ++i)
+		for (unsigned j = 0; j < Y_MAX; ++j)
+			for (unsigned w = 0; w < W_MAX; ++w)
+				lane[i][j].emplace_back(A[i][j][w]);
+
+	std::array<std::vector<char>, Y_MAX> plane;
+	for (unsigned j = 0; j < Y_MAX; ++j)
+		for (unsigned i = 0; i < X_MAX; ++i)
+			plane[j].insert(plane[j].end(), lane[i][j].begin(), lane[i][j].end());
+
+	for (unsigned i = 0; i < X_MAX; ++i)
+		str.insert(str.end(), plane[i].begin(), plane[i].end());
+
+	return std::move(str);
+}
+
 void sha3::keccakPermutation()
 {
 	// TODO przekonwertowaæ S na A - prawdopodobnie w zewnêtrznej funkcji
@@ -53,7 +82,8 @@ sha3::sha3()
 
 sha3::sha3(int size)
 {
-	// TODO init
+//	A.reserve(5);
+//	std::for_each(A.begin(), A.end(), [](std::vector<std::vector<uint8_t>> &a2Dim) -> void { a2Dim.reserve(Y_MAX); });
 }
 
 
@@ -63,5 +93,15 @@ sha3::~sha3()
 
 std::string sha3::compute(const std::vector<char>& data)
 {
+//	std::vector<char> ldata(data.begin(), data.begin() + 1600);
+//	for (char it : ldata)
+//		std::cout << it;
+//
+//	std::cout << std::endl;
+//
+//	convertStringToStateArray(ldata);
+//	for (char it : convertStateArrayToString())
+//		std::cout << it;
+
 	return "abcde";
 }
--- sha3dll/sha3.h
+++ sha3dll/sha3.h
@@ -2,15 +2,25 @@
 #define __SHA3_H__
 
 #include "HashStrategy.h"
+#include <algorithm>
+#include <memory>
+#include <array>
+#include <iostream>
 
 class sha3 :
 	public HashStrategy
 {
 private:
 	static const unsigned int NUMBER_OF_ROUNDS = 24;
+	static const int X_MAX = 5;
+	static const int Y_MAX = 5;
+	static const int W_MAX = 64;
 
-	std::vector<uint8_t> A;
+	//std::vector<std::vector<std::vector<uint8_t>>> A;
+	std::array<std::array<std::array<uint8_t, W_MAX>, Y_MAX>, X_MAX> A;
 
+	void convertStringToStateArray(const std::vector<char>& str);
+	std::vector<char> convertStateArrayToString();
 
 	void keccakPermutation();
 	void keccakTheta();
